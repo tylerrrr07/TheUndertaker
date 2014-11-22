89b9d88ad8a9d0a7bf40f299e8e8ecfb644094c0
SpartanDonut <tylerrrr07@gmail.com>
2014-11-22 00:38:32 -0600

    First Person Stuff Added

---
+++ Unity/Assets/Scripts/GrabberController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabberController : MonoBehaviour {
+
+    public float Reach;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Unity/Assets/Scripts/Rotation.cs
+++ Unity/Assets/Scripts/Rotation.cs
@@ -1,21 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Rotation : MonoBehaviour {
 
+    public Camera PlayerCamera;
+    private bool isGrabbed = false;
 	private Transform tr;
 	private Rigidbody rb;
 	private bool collision = false;
+    private Vector2 centerOfScreen;
+    public List<GameObject> GrabbableObjects;
+    private Vector2 targetPosition;
+    public GrabberController PlayerGrabberrController;
+    public float ReachDistance = 5;
+    public float ReachLength = 3.0f;
 
-
+    public float GrabOffsetY = 1.0f;
 	// Use this for initialization
 	void Start () {
-
+        Physics.IgnoreLayerCollision(8, 9, true);
+        ReachDistance = PlayerGrabberrController.Reach;
+        centerOfScreen = new Vector2(Screen.width / 2, (float)((Screen.height / 2)));
+        targetPosition = new Vector2(Screen.width / 2, (float)((Screen.height / 2) + (Screen.height * .2)));
 		tr = transform;
 		rb = rigidbody;
 
 		// set floor black
-		GameObject.Find("Floor").renderer.material.color = Color.blue;
+		//GameObject.Find("Floor").renderer.material.color = Color.blue;
 	}
 	
 	// Update is called once per frame
@@ -26,30 +38,50 @@ public class Rotation : MonoBehaviour {
 		}
 
 
-		if ( Input.GetMouseButton(0) ){
-
-			Vector3 temp = Input.mousePosition;
-			temp.z = 10.0f;
-			tr.position = Camera.main.ScreenToWorldPoint(temp);
-			rb.useGravity = false;
-			rb.velocity = Vector3.zero;
+		if ( Input.GetMouseButton(0) && !isGrabbed){
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(centerOfScreen);
+            if (Physics.Raycast(ray, out hitInfo, ReachDistance))
+            {
+                foreach (var GrabbableObject in GrabbableObjects)
+                {
+                    if (hitInfo.transform == GrabbableObject.transform)
+                    {                        
+                        isGrabbed = true;
+                        break;
+                    }
+                }
+            }
 //
 //			Debug.Log (temp);
 
-		} else {
-
-
-//			Vector3 temp = new Vector3(-0.45f, 10f, -1.1f); 
-//			tr.position = temp;
-//			rb.useGravity = false;
-//			rb.velocity = Vector3.zero;
-		
-
-			rb.useGravity = true;	
-
 		}
+        else if (isGrabbed && Input.GetMouseButton(0))
+        {
+            HandleGrabbed();
+        }
+        else
+        {
+            isGrabbed = false;
+            rb.useGravity = true;	
+        }
 	
 	}
+
+    private void HandleGrabbed()
+    {
+        Debug.DrawRay(PlayerCamera.transform.position, PlayerCamera.transform.forward);
+        Vector3 temp = PlayerCamera.transform.position + PlayerCamera.transform.forward * ReachLength;
+        temp.y = temp.y + GrabOffsetY;
+        Debug.Log(PlayerCamera.transform.position.ToString());
+        
+        Debug.DrawLine(new Vector3(PlayerCamera.transform.position.x, targetPosition.y, PlayerCamera.transform.position.z), temp);
+
+        tr.position = temp;
+        tr.rotation = new Quaternion(0.0f, PlayerCamera.transform.rotation.y, 0.0f, PlayerCamera.transform.rotation.w);
+        rb.useGravity = false;
+        rb.velocity = Vector3.zero;
+    }
 
 	void OnCollisionEnter() {
 		collision = true;
@@ -62,4 +94,4 @@ public class Rotation : MonoBehaviour {
 
 
 
-}
+};
