949c418f31473c76ef447633130e15ffaaa6ee82
SpartanDonut <tylerrrr07@gmail.com>
2014-11-21 19:11:12 -0600

    Added scripts and test scene for detecting proper number of items within a given area; Overlfow detection has also been implemented and works with these scripts;

---
+++ Unity/Assets/Scripts/CoffinContentsManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoffinContentsManager : MonoBehaviour {
+
+    public ObjectsIntersectingColliderTracker InBoundsCollider;
+    public ObjectsIntersectingColliderTracker OutOfBoundsCollider;
+    public int ItemsToSucceed = 5;
+    public bool IsComplete = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (!IsComplete && OutOfBoundsCollider.NumberOfObjectsIntersecting == 0 && InBoundsCollider.NumberOfObjectsIntersecting == ItemsToSucceed)
+        {
+            Debug.Log("Great Success!");
+            IsComplete = true;
+        }
+
+	}
+}
---
+++ Unity/Assets/Scripts/ObjectsIntersectingColliderTracker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectsIntersectingColliderTracker : MonoBehaviour {
+
+    public int NumberOfObjectsIntersecting = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        NumberOfObjectsIntersecting += 1;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        NumberOfObjectsIntersecting -= 1;
+    }
+}
